c6452c2dce43123ed9778ae9fab57dd9baaf8715
Junaid Farooq <junaid@evercam.io>
2020-03-07 23:20:20 +0500

    Start creating users.

--- lib/analysis_web/router.ex
+++ lib/analysis_web/router.ex
@@ -3,9 +3,13 @@ defmodule AnalysisWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug CORSPlug, origin: ["*"]
   end
 
   scope "/api", AnalysisWeb do
     pipe_through :api
+
+    post "/users/register", UserController, :create
+    options "/users/register", UserController, :nothing
   end
 end
